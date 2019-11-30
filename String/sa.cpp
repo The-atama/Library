943449a7ddd0d13bff1e228882ae517cc542ad9b
okuraofvegetable <okuraofvegetable@gmail.com>
2019-12-01 00:56:48 +0900

    fix Suffix Array

--- String/sa.cpp
+++ String/sa.cpp
@@ -1,86 +1,89 @@
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <cmath>
-#include <complex>
-#include <string>
-#include <sstream>
-#include <algorithm>
-#include <vector>
-#include <queue>
-#include <stack>
-#include <functional>
-#include <iostream>
-#include <map>
-#include <set>
+#include <bits/stdc++.h>
 using namespace std;
-typedef pair<int,int> P;
-typedef long long ll;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-#define pu push
-#define pb push_back
-#define mp make_pair
-#define eps 1e-9
-#define INF 2000000000
-#define sz(x) ((int)(x).size())
-#define fi first
-#define sec second
-#define SORT(x) sort((x).begin(),(x).end())
-#define all(x) (x).begin(),(x).end()
-#define EQ(a,b) (abs((a)-(b))<EPS)
-string s;
-int sa[100100],rank[100100],tmp[100100],lcp[100100];
-int n,k;
-bool compare_sa(int i,int j)
-{
-	if(rank[i]!=rank[j])return rank[i]<rank[j];
-	else
-	{
-		int ri=(i+k<=n)?rank[i+k]:-1;
-		int rj=(j+k<=n)?rank[j+k]:-1;
-		return ri<rj;
-	}
-}
-void construct_sa()
-{
-	n=s.size();
-	for(int i=0;i<=n;i++)
-	{
-		rank[i]=(i<n)?s[i]:-1;
-		sa[i]=i;
-	}
-	for(k=1;k<=n;k*=2)
-	{
-		sort(sa,sa+n+1,compare_sa);
-		tmp[sa[0]]=0;
-		for(int i=1;i<=n;i++)tmp[sa[i]]=tmp[sa[i-1]]+(compare_sa(sa[i-1],sa[i])?1:0);
-		for(int i=0;i<=n;i++)rank[i]=tmp[i];
-	}
-	return;
-}
-void construct_lcp()
-{
-	for(int i=0;i<=n;i++)rank[sa[i]]=i;
-	int h=0;
-	lcp[0]=0;
-	for(int i=0;i<n;i++)
-	{
-		int j=sa[rank[i]-1];
-		if(h>0)h--;
-		for(;i+h<n&&j+h<n;h++)if(s[i+h]!=s[j+h])break;
-		lcp[rank[i]-1]=h;
-	}
-	return;
-}
-int main()
-{
+struct SuffixArray{
+  string s;
+  vector<int> sa;
+  vector<int> rank;
+  vector<int> lcp;
+  explicit SuffixArray(string s):s(s){
+    int n = s.size();
+    sa.resize(n+1);
+    rank.resize(n+1);
+    vector<int> tmp(n+1);
+    for(int i=0;i<=n;i++){
+      rank[i] = (i<n)?s[i]:-1;
+      sa[i] = i;
+    }
+    for(int k=1;k<=n;k*=2){
+      auto compare_sa = 
+        [&](const int &i,const int &j){
+           if(rank[i]!=rank[j])return rank[i]<rank[j];
+           else{
+             int ri=(i+k<=s.size())?rank[i+k]:-1;
+             int rj=(j+k<=s.size())?rank[j+k]:-1;
+             return ri<rj;
+           }
+        };
+      sort(sa.begin(),sa.end(),compare_sa);
+      tmp[sa[0]]=0;
+      for(int i=1;i<=n;i++)tmp[sa[i]]=tmp[sa[i-1]]+(compare_sa(sa[i-1],sa[i])?1:0);
+      for(int i=0;i<=n;i++)rank[i]=tmp[i];
+    }
+  }
+  size_t size() const {
+    return s.size();
+  }
+  int operator [] (int id) const {
+    return sa[id];
+  }
+};
+struct LongestCommonPrefix{
+  const SuffixArray &sa;
+  vector<int> lcp,rank;
+  LongestCommonPrefix(const SuffixArray &sa):sa(sa){
+    int n = sa.size();
+    lcp.resize(sa.size()+1);
+    rank.resize(sa.size()+1);
+    for(int i=0;i<=sa.size();i++){
+      rank[sa[i]]=i;
+    }
+    int h = 0;
+    lcp[0] = 0;
+    for(int i=0;i<sa.size();i++){
+      int j = sa[rank[i]-1];
+      if(h>0)h--;
+      for(;i+h<n&&j+h<n;h++)if(sa.s[i+h]!=sa.s[j+h])break;
+      lcp[rank[i]-1] = h;
+    }
+  }
+  int operator [] (int id) const {
+    assert(id>=0&&id<lcp.size());
+    return lcp[id];
+  }
+};
+
+struct BurrowsWheelerTransform{
+  vector<char> bwt;
+  const SuffixArray &sa;
+  BurrowsWheelerTransform(const SuffixArray &sa):sa(sa){
+    bwt.resize(sa.size()+1);
+    for(int i=0;i<bwt.size();i++){
+      if(sa[i]==0)bwt[i] = '$'; // dummy
+      else bwt[i] = sa.s[sa[i]-1];
+    }
+  }
+  char operator [] (int id) const {
+    return bwt[id];
+  }
+};
+
+int main(){
+  string s;
 	cin >> s;
-	construct_sa();
-	construct_lcp();
-	for(int i=0;i<=n;i++)
-	{
-		cout << sa[i]  <<  ' ' << lcp[i] << endl;
+	SuffixArray sa(s);
+  LongestCommonPrefix lcp(sa);
+	for(int i=0;i<=s.size();i++){
+		cout << setw(3) << sa[i] << ' ' << lcp[i] <<  ' ' << s.substr(sa[i]) << endl;
 	}
 	return 0;
 }
