c16519883ab9c1ae3b7348ab3e221f727864c87c
okuraofvegetable <okuraofvegetable@gmail.com>
2020-02-09 17:57:05 +0900

    add ntt(non recursive) and matrix

--- Math/matrix.cpp
+++ Math/matrix.cpp
@@ -1,85 +1,108 @@
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <cmath>
-#include <complex>
-#include <string>
-#include <sstream>
-#include <algorithm>
-#include <numeric>
-#include <vector>
-#include <queue>
-#include <stack>
-#include <functional>
-#include <iostream>
-#include <map>
-#include <set>
+#include <bits/stdc++.h>
+
 using namespace std;
-typedef pair<int,int> P;
+
 typedef long long ll;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-#define pu push
-#define pb push_back
-#define mp make_pair
-#define eps 1e-9
-#define INF 2000000000
-#define sz(x) ((int)(x).size())
-#define fi first
-#define sec second
-#define SORT(x) sort((x).begin(),(x).end())
-#define all(x) (x).begin(),(x).end()
-#define rep(i,n) for(int (i)=0;(i)<(int)(n);(i)++)
-#define repn(i,a,n) for(int (i)=(a);(i)<(int)(n);(i)++)
-#define EQ(a,b) (abs((a)-(b))<eps)
 
-typedef vector<ll> vec;
-typedef vector<vec> mat;
-
-ll MOD;
-
-mat mul(mat &A,mat &B)
-{
-	mat C(A.size(),vec(B[0].size()));
-	for(int i=0;i<A.size();i++)
-	{
-		for(int k=0;k<B.size();k++)
-		{
-			for(int j=0;j<B[0].size();j++)
-			{
-				C[i][j]=(C[i][j]+A[i][k]*B[k][j])%MOD;
-			}
-		}
-	}
-	return C;
-}
-
-mat pow(mat A, ll n)
-{
-	mat B(A.size(),vec(A.size()));
-	for(int i=0;i<A.size();i++)
-	{
-		B[i][i]=1ll;
-	}
-	while(n>0)
-	{
-		if(n&1)B = mul(B,A);
-		A = mul(A,A);
-		n >>= 1;
-	}
-	return B;
-}
+// matrix class
+// constructor of T : T(1),T(0) must be unit of T
+// verified DDCC B https://atcoder.jp/contests/ddcc2020-final/submissions/9941022
+template <class T>
+struct Matrix{
+  int H,W;
+  vector<vector<T>> elem;
+  Matrix(int H,int W,T val = T(0)):H(H),W(W){
+    elem.resize(H);
+    for(int i=0;i<H;i++)elem[i].assign(W,val);
+  }
+  Matrix(const vector<vector<T>> &m) : H(m.size()), W(m.size() ? m[0].size() : 0) {
+    elem.resize(H);
+    for(int i=0;i<H;i++)elem[i].resize(W);
+    for(int i=0;i<H;i++) elem[i] = m[i];
+  }
+  vector<T>& operator [] (int h) {
+    return elem[h];
+  }
+  static Matrix identity(int N) {
+    Matrix a(N,N);
+    for(int i=0;i<N;i++){
+      for(int j=0;j<N;j++){
+        if(i==j)a.elem[i][i] = T(1);
+        else a.elem[i][j] = T(0);
+      }
+    }
+    return a;
+  }
+  Matrix operator + (const Matrix &m) const {
+    assert( H==m.H && W==m.W );
+    Matrix a(H,W);
+    for(int i=0;i<H;i++){
+      for(int j=0;j<W;j++){
+        a.elem[i][j] = this->elem[i][j] + m.elem[i][j];
+      }
+    }
+    return a;
+  }
+  Matrix operator - (const Matrix &m) const {
+    assert( H==m.H && W==m.W );
+    Matrix a(H,W);
+    for(int i=0;i<H;i++){
+      for(int j=0;j<W;j++){
+        a.elem[i][j] = this->elem[i][j] - m.elem[i][j];
+      }
+    }
+    return a;
+  }
+  Matrix operator * (const Matrix &m) const {
+    assert( W == m.H );
+    Matrix a(H,m.W);
+    for(int i=0;i<H;i++){
+      for(int j=0;j<m.W;j++){
+        for(int k=0;k<W;k++){
+          a.elem[i][j] = a.elem[i][j] + (this->elem[i][k] * m.elem[k][j]);
+        }
+      }
+    }
+    return a;
+  }
+  Matrix &operator+= (const Matrix &m) { return *this = *this + m; }
+  Matrix &operator-= (const Matrix &m) { return *this = *this - m; }
+  Matrix &operator*= (const Matrix &m) { return *this = *this * m; }
+  Matrix pow(long long x) const {
+    Matrix a = identity(H);
+    Matrix b = *this;
+    while(x){
+      if(x&1)a *= b;
+      b *= b;
+      x >>= 1;
+    }
+    return a;
+  }
+  friend ostream & operator << (ostream &os, const Matrix &x) {
+    for(int i=0;i<x.H;i++){
+      for(int j=0;j<x.W;j++){
+        os << x.elem[i][j];
+        if(j+1<x.W)cout << ' ';
+      }
+      if(i+1<x.H)cout << endl;
+    }
+    return os;
+  }
+  friend istream & operator >> (istream &is, Matrix &x) {
+    for(auto &v : x.elem) is >> v;
+    return is;
+  }
+};
 
 // 1 1 2 3 5 8 ...
-ll fib(ll x)
-{
-	mat A(2,vec(2));
-	A[0][0]=1;A[0][1]=1;A[1][0]=1;A[1][1]=0;
-	A = pow(A,x);
-	return A[1][0];
+ll fib(ll x){
+  Matrix<ll> A(2,2);
+  A[0][0]=1;A[0][1]=1;A[1][0]=1;A[1][1]=0;
+  A = A.pow(x);
+  return A[1][0];
 }
 
-int main()
-{
-	return 0;
+int main(){
+  cout << fib(30) << endl;
+  return 0;
 }
